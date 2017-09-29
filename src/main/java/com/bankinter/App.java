a3c0baeaf6cea3b156a3edd3e4a966d9e84c7db0
mnnieto <mnnieto@gmail.com>
2017-09-29 18:00:30 +0200

    'test'

---
+++ src/main/java/com/bankinter/App.java
@@ -0,0 +1,13 @@
+package com.bankinter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
